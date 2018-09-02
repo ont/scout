1cf045507fd12d64b80c871c44d1f6cdebd47385
ont <ont.rif@gmail.com>
2018-09-02 19:22:35 +0700

    (f) forgotten storage.Close()

--- src/scout/storage-rotate.go
+++ src/scout/storage-rotate.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"sync"
 	"time"
 )
@@ -38,6 +39,9 @@ func (s *StorageRotate) Rotate(stamp time.Time) {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 
+	if err := s.storage.Close(); err != nil {
+		log.Fatal("Can't close storage during rotation:", err)
+	}
 	s.storage = s.fabric(stamp.Format("2006-01-02_15:04:05"))
 }
 
